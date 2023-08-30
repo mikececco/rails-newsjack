dcd3e7a21e035508c1a9d3026bcbb8b3fb9c2289
Mike Cecconello <mikececco2000@gmail.com>
2023-08-30 20:26:53 +0200

    Twitter trends test

--- twitter_api_request.rb
+++ twitter_api_request.rb
@@ -10,7 +10,7 @@ http.use_ssl = true
 
 request = Net::HTTP::Post.new(url)
 request["content-type"] = 'application/x-www-form-urlencoded'
-request["X-RapidAPI-Key"] = ENV['RAPID_KEY']
+request["X-RapidAPI-Key"] = '125f259f4cmsh1f8cf935dd0c7fdp1b7e50jsn26f913fab809'
 request["X-RapidAPI-Host"] = 'twitter-trends5.p.rapidapi.com'
 
 world = "woeid=1"
@@ -26,7 +26,6 @@ data = response_dict["trends"]
 
 # Extract "name" and "volume" attributes from each hash
 name_volume_list = []
-
 data.each do |key, value|
   name = value["name"]
   volume = value["volume"]
