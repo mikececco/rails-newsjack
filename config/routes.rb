b1acef55996d983cdbc5a298eeebdbc27c87e55e
Mike Cecconello <mikececco2000@gmail.com>
2023-09-19 01:23:47 +0200

    Added payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get 'blog', to: 'pages#blog'
+  get 'early_access', to: 'pages#early_access'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
